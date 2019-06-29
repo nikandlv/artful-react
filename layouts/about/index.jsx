7b703238007bb871f77c3dfd995622c8b888d832
Nikan Dalvand <nikandalvand@gmail.com>
2019-06-29 15:18:41 +0430

    Started about section and added react-flexbox-grid

---
+++ layouts/about/index.jsx
@@ -0,0 +1,8 @@
+export default function About() {
+    return (
+        <section className="about-section">
+            <h4 className="section-title">How it works</h4>
+            <h2>Three Simple Steps To Start The Working Process</h2>
+        </section>
+    )
+}
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,11 @@
 import Wrapper from '../components/Wrapper';
 import Header from '../layouts/header';
+import About from '../layouts/about';
 export default function Index() {
 	return (
 		  <Wrapper>
 			  <Header />
+			  <About />
 		  </Wrapper>
 
 	);
