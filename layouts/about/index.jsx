269d404321dfe1ff8a92c6cd2a4b08dba6eeb885
Nikan Dalvand <nikandalvand@gmail.com>
2019-06-29 18:27:21 +0430

    About row is now more dense

--- layouts/about/index.jsx
+++ layouts/about/index.jsx
@@ -5,18 +5,22 @@ export default function About() {
         <section className="about-section">
             <h4 className="section-title">How it works</h4>
             <h2>Three Simple Steps To Start The Working Process</h2>
-            <div className="row center-xs about-row">
-                <div className="col-xs-12 col-sm-4">
-                    <p className="title">Research Project</p>
-                    <p className="subtitle">There are many variations of passages of Lorem Ipsum available</p>
-                </div>
-                <div className="col-xs-12 col-sm-4">
-                    <p className="title">Targeting</p>
-                    <p className="subtitle">There are many variations of passages of Lorem Ipsum available</p>
-                </div>
-                <div className="col-xs-12 col-sm-4">
-                    <p className="title">Result</p>
-                    <p className="subtitle">There are many variations of passages of Lorem Ipsum available</p>
+            <div className="row center-xs">
+                <div className="col-xs-12 col-sm-10">
+                    <div className="row center-xs about-row">
+                        <div className="col-xs-12 col-sm-4">
+                            <p className="title">Research Project</p>
+                            <p className="subtitle">There are many variations of passages of Lorem Ipsum available</p>
+                        </div>
+                        <div className="col-xs-12 col-sm-4">
+                            <p className="title">Targeting</p>
+                            <p className="subtitle">There are many variations of passages of Lorem Ipsum available</p>
+                        </div>
+                        <div className="col-xs-12 col-sm-4">
+                            <p className="title">Result</p>
+                            <p className="subtitle">There are many variations of passages of Lorem Ipsum available</p>
+                        </div>
+                    </div>
                 </div>
             </div>
             <Art />
